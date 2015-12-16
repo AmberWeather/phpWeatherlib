5c321eb0de6c8aa2319227b3ad6821c4813c869b
DropFan <DropFan@Gmail.com>
2015-12-16 12:18:35 +0800

    add main class

---
+++ Weatherlib.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @author: Tiger <DropFan@Gmail.com>
+ * @date: 2015/12/12
+ */
+namespace Weatherlib;
+
+use Weatherlib\Model\Location;
+use Weatherlib\Model\Current;
+use Weatherlib\Model\DailyForecastList;
+use Weatherlib\Model\HourlyForecastList;
+use Weatherlib\Util\Base;
+
+use Weatherlib\Provider\ProviderFactory;
+
+/**
+*
+*/
+class Weatherlib {
+
+    public $location;
+    private $provider;
+    public $currentCondition;
+    public $dailyForecast;
+    public $hourlyForecast;
+    public $sun_moon;
+
+    public function __construct($locationData = [], $datasource = 'wunderground') {
+        $this->setLocation($locationData);
+        $this->setProvider($datasource, $location = $this->location);
+    }
+
+    public function setProvider($datasource = 'wunderground') {
+        $this->provider = ProviderFactory::getProvider($datasource, $this->location);
+    }
+
+    public function setLocation($data = []) {
+        // $data = [
+        //         'id' => '1',
+        //         'city' => 'Beijing',
+        //         'latitude' => '39.55',
+        //         'longitude' => '119.55',
+        //         'country' => 'China',
+        //         'countryCode' => 'CN',
+        //         'lang' => 'zh_CN',
+        //         'full_text' => 'Beijing, China',
+        //         'timezone' => '+8',
+        //         'timezone_full' => 'Asia/Beijing',
+        //         'osmid' => '',
+        //         'accuid' => '',
+        //         'owmid' => '',
+        //         'yhcode' => '',
+        //         'wmo' => '',
+        //         'zipcode' => '100000'
+        //         ];
+        $this->location = new Location($data);
+    }
+
+    public function getLocation() {
+        return $this->location;
+    }
+
+    public function fetchWeather() {
+        if ($this->provider->fetchRaw()) {
+            $this->currentCondition = $this->provider->getCurrentCondition();
+            $this->dailyForecast = $this->provider->getDailyForecast();
+            $this->hourlyForecast = $this->provider->getHourlyForecast();
+            $this->sun_moon = $this->provider->getSunAndMoon();
+            return true;
+        }
+        return false;
+    }
+
+    public function getRawData() {
+        return $this->provider->getRawData();
+    }
+    public function getCurrentCondition() {
+        if (!($this->currentCondition instanceof Current)) {
+            $this->currentCondition = $this->provider->getCurrentCondition();
+        }
+        return $this->currentCondition;
+    }
+
+    public function getDailyForecast() {
+        if (!($this->dailyForecast instanceof DailyForecastList)) {
+            $this->dailyForecast = $this->provider->getDailyForecast();
+        }
+        return $this->dailyForecast;
+    }
+
+    public function getHourlyForecast() {
+        if (!($this->hourlyForecast instanceof HourlyForecastList)) {
+            $this->hourlyForecast = $this->provider->getHourlyForecast();
+        }
+        return $this->hourlyForecast;
+    }
+}
