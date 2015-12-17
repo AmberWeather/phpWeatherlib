f4fbd7c6b74742532854aaf6c66dc298e53b70c6
DropFan <DropFan@Gmail.com>
2015-12-17 11:55:29 +0800

    add some comments in ProviderFactory class

--- Provider/ProviderFactory.php
+++ Provider/ProviderFactory.php
@@ -10,14 +10,23 @@ use Weatherlib\Provider\WundergroundProvider;
 use Weatherlib\Model\Location;
 
 /**
-*
-*/
+ * This clss is a creater of provider.
+ * You should add a new provider in getProvier() method if you want to use other provider class.
+ * @author: Tiger <DropFan@Gmail.com>
+ * @date: 2015/12/12
+ */
 class ProviderFactory {
 
     private function __construct() {
         return false;
     }
 
+    /**
+     * [getProvider description]
+     * @param  string $provider which provider object you want
+     * @param  object $location object of Location
+     * @return object           object of Provier
+     */
     public static function getProvider($provider = 'wunderground', $location) {
 
         if (!($location instanceof Location)) {
