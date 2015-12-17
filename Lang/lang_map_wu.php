74a25f71e376ab7afc0df1d568290c91cd595ebe
DropFan <DropFan@Gmail.com>
2015-12-17 10:59:43 +0800

    add lang_map_wu.php

---
+++ Lang/lang_map_wu.php
@@ -0,0 +1,97 @@
+<?php
+/*
+* Map iso language code to weather underground code.
+*
+* refer to:
+*   http://www.wunderground.com/weather/api/d/docs?d=language-support&MR=1
+*   https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+*   https://android.googlesource.com/platform/packages/apps/Settings/+/android-6.0.0_r13/res/
+*/
+
+$locales_map_wu = array(
+    'af_' => 'AF',
+    'sq_' => 'AL',
+    'ar_' => 'AR',
+    'hy_' => 'HY',
+    'az_' => 'AZ',
+    'be_' => 'BY',
+    'bg_' => 'BU',
+
+    'my_' => 'MY',
+    'ca_' => 'CA',
+
+    'eu_' => 'EU',
+    'en_' => 'EN',
+    'en_GB' => 'LI',
+
+    'zh_' => 'TW',
+    'zh_CN' => 'CN',
+
+    'hr_' => 'CR',
+    'cs_' => 'CZ',
+    'da_' => 'DK',
+    'dv_' => 'DV',
+    'nl_' => 'NL',
+    'eo_' => 'EO',
+    'et_' => 'ET',
+    'fa_' => 'FA',
+    'fi_' => 'FI',
+    'fr_' => 'FR',
+    'fr_CA' => 'FC',
+    'gl_' => 'GZ',
+    'de_' => 'DL',
+    'ka_' => 'KA',
+    'el_' => 'GR',
+    'gu_' => 'GU',
+    'ht_' => 'HT',
+
+    'iw_' => 'IL',
+    'he_' => 'IL',
+
+    'hi_' => 'HI',
+    'hu_' => 'HU',
+    'is_' => 'IS',
+    'io_' => 'IO',
+    'id_' => 'ID',
+    'ga_' => 'IR',
+    'it_' => 'IT',
+    'ja_' => 'JP',
+    'jv_' => 'JW',
+    'km_' => 'KM',
+    'ko_' => 'KR',
+    'ku_' => 'KU',
+    'la_' => 'LA',
+    'lv_' => 'LV',
+    'lt_' => 'LT',
+    'mk_' => 'MK',
+    'mt_' => 'MT',
+    'mi_' => 'MI',
+    'mr_' => 'MR',
+    'mn_' => 'MN',
+    'no_' => 'NO',
+    'oc_' => 'OC',
+    'ps_' => 'PS',
+    'pl_' => 'PL',
+    'pt_' => 'BR',
+    'pa_' => 'PA',
+    'ro_' => 'RO',
+    'ru_' => 'RU',
+    'sr_' => 'SR',
+    'sk_' => 'SK',
+    'sl_' => 'SL',
+    'es_' => 'SP',
+    'sw_' => 'SI',
+    'sv_' => 'SW',
+    'tl_' => 'TL',
+    'th_' => 'TH',
+    'tr_' => 'TR',
+    'tk_' => 'TK',
+    'uk_' => 'UA',
+    'uz_' => 'UZ',
+    'vi_' => 'VU',
+    'cy_' => 'CY',
+    'wo_' => 'SN',
+    'yi_' => 'YI'
+);
+
+return $locales_map_wu;
